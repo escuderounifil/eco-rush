c0075447833c82fee7b75fbdabf35d9422b22422
scodero <guiescudero2003@GMAIL.COM>
2026-08-23 14:28:38 -0300

    menu arrumado pós conflict

--- options.gd
+++ options.gd
@@ -1,10 +1,13 @@
 extends Control
 
 @onready var musica: AudioStreamPlayer = $Musica
+@onready var som_click: AudioStreamPlayer =$SomClick
 
 var button_type = null
 
 func _on_texture_button_pressed() -> void:
+	som_click.play()
+	
 	button_type = "start"
 	$ColorRect.show()
 	$ColorRect/Fade_timer.start()
